091926b5d5ac7debfc62368796b01005cadd13d7
Vinicius Azevedo <vinicius.azevedo@fhinck.com>
2018-04-05 10:16:18 -0300

    atualização

--- assets/js/core.js
+++ assets/js/core.js
@@ -1,6 +1,21 @@
-        var options = {
-            strings: ["Meu nome é </br>Vinícius Azevedo </br> e eu faço coisas na internet! </br></br></br> Vamos transformar o seu <span class='sonho'>Sonho</span> em <span class='realidade'> Realidade </span>?"],
-            typeSpeed: 40
-        }
+var options = {
+    strings: ["Meu nome é </br>Vinícius Azevedo </br> e eu faço coisas na internet! </br></br></br> Vamos transformar o seu <span class='sonho'>Sonho</span> em <span class='realidade'> Realidade </span>?"],
+    typeSpeed: 45
+}
 
-        var typed = new Typed(".typed", options);
+var typed = new Typed(".typed", options);
+
+
+let sobreMimTxt = 0;
+
+$(".sobreMim").click(() => {
+    // $('.textoSobreMim').toggleClass(".blur");
+    if (sobreMimTxt == 0) {
+        $("#sobreMimTxT").removeClass("blur");
+        $(".sobreMim").css("display","none");
+        sobreMimTxt = 1;
+    } else {
+        $("#sobreMimTxT").addClass("blur");
+        sobreMimTxt = 0;
+    }
+});
